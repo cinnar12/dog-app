cc58d2a93309a46138d76e8a4cc2ee8c45a65181
Tho Tran <cinnar12@gmail.com>
2017-12-01 23:44:14 -0500

    7
    added rough keyboard

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@ $(document).ready(function(){
 $("#navicon").click(function(){
   $("nav").slideToggle();
 });
+
+$("#corgi").hover( function() {
+  $("#att1").fadeToggle(300);
+});
+
+$("#pug").hover( function() {
+  $("#att2").fadeToggle(300);
+});
+
+$("#husky").hover( function() {
+  $("#att3").fadeToggle(300);
+});
+
+$("#shiba").hover( function() {
+  $("#att4").fadeToggle(300);
+});
+
+$("#pom").hover( function() {
+  $("#att5").fadeToggle(300);
+});
+
+$("#golden").hover( function() {
+  $("#att6").fadeToggle(300);
+});
+
+$("#chihuahua").hover( function() {
+  $("#att7").fadeToggle(300);
+});
