ae0be3c76aca7d4d888a004c37cda8ceac45e1b3
Tho Tran <cinnar12@gmail.com>
2017-12-05 10:37:27 -0500

    9
    added borks; fixed

--- js/home.js
+++ js/home.js
@@ -2,19 +2,6 @@ $(document).ready(function(){
   $(".wrapper").fadeIn(750);
 });
 
-$("#navicon").click(function(){
-  $("nav").slideToggle();
-});
-
-$("#gabe").click(function(){
-  $("#gabethedog").slideToggle();
-});
-
-$("#sploot").click(function(){
-  $("#splootex").slideToggle();
-  $("#splootex2").slideToggle();
-});
-
 //attribution
 $("#corgi").hover( function() {
   $("#att1").fadeToggle(300);
---
+++ js/lingo.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $(".wrapper").fadeIn(750);
+});
+
+$("#navicon").click(function(){
+  $("nav").slideToggle();
+});
+
+$("#gabe").click(function(){
+  $("#gabethedog").slideToggle();
+});
+
+$("#sploot").click(function(){
+  $("#splootex").slideToggle();
+  $("#splootex2").slideToggle();
+});
